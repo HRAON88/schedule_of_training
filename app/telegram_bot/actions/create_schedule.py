1a29fa13fb4bdd9fbb48a157929b9bd94e7af96a
Denis <isayan1998@gmail.com>
2024-09-03 12:36:32 +0300

    refactoring - create KeyBoardFactory

--- app/telegram_bot/actions/create_schedule.py
+++ app/telegram_bot/actions/create_schedule.py
@@ -14,6 +14,7 @@ from app.schemes.callback_data import CallBackData
 from app.services.user_flow_admin import UserFlowAdmin
 from app.telegram_bot.settings import END_ROUTES, START_ROUTES
 from app.user_flow_storage import user_flow_storage
+from app.utils.keyboard import KeyBoardFactory
 
 
 async def create_schedule_step_1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -22,19 +23,16 @@ async def create_schedule_step_1(update: Update, context: ContextTypes.DEFAULT_T
     with Connection() as con:
         repository = SportsRepository(con)
         sports = repository.get_all()
-    keyboard = [[]]
+    keyboard = KeyBoardFactory()
     user = update.callback_query.from_user
     user_flow_storage[user.id] = {}
     for sport in sports:
         trace_id = uuid.uuid4().hex
         data = {"sport": sport.to_dict()}
-        button = InlineKeyboardButton(
-            sport.sport, callback_data=CallBackData(tag="cs_1", trace_id=trace_id).model_dump_json()
-        )
-        keyboard[0].append(button)
+        keyboard.add_item(sport.sport, "cs_1", trace_id)
         user_flow_storage[user.id][trace_id] = data
-    keyboard.append([InlineKeyboardButton("Вернуться в меню", callback_data=CallBackData(tag="back_to_menu").model_dump_json())])
-    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    reply_markup = InlineKeyboardMarkup(keyboard.generate())
     await query.edit_message_text(text="Выберете вид спорта", reply_markup=reply_markup)
     return START_ROUTES
 
@@ -49,28 +47,15 @@ async def create_schedule_step_2(update: Update, context: ContextTypes.DEFAULT_T
         datetime.date(year=today.year, month=today.month, day=day).strftime("%d.%m.%Y")
         for day in range(today.day, monthrange(today.year, today.month)[1] + 1)
     ]
-    keyboard = []
-    col_count = 4
-    index = 0
-    for _ in range((len(days) // col_count) + 1):
-        row = []
-        for _ in range(col_count):
-            if index >= len(days):
-                break
-            trace_id = uuid.uuid4().hex
-            if trace_id not in user_flow_storage[user.id]:
-                user_flow_storage[user.id][trace_id] = {}
-            user_flow_storage[user.id][trace_id].update(cache_data)
-            user_flow_storage[user.id][trace_id]["day"] = days[index]
-            row.append(
-                InlineKeyboardButton(
-                    days[index], callback_data=CallBackData(tag="cs_2", trace_id=trace_id).model_dump_json()
-                )
-            )
-            index += 1
-        keyboard.append(row)
-    keyboard.append([InlineKeyboardButton("Вернуться в меню", callback_data=CallBackData(tag="back_to_menu").model_dump_json())])
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    keyboard = KeyBoardFactory()
+    for item in days:
+        trace_id = uuid.uuid4().hex
+        if trace_id not in user_flow_storage[user.id]:
+            user_flow_storage[user.id][trace_id] = {}
+        user_flow_storage[user.id][trace_id].update(cache_data)
+        user_flow_storage[user.id][trace_id]["day"] = item
+        keyboard.add_item(item, "cs_2", trace_id)
+    reply_markup = InlineKeyboardMarkup(keyboard.generate())
     await query.edit_message_text(text="Выберете дату", reply_markup=reply_markup)
     return START_ROUTES
 
@@ -86,28 +71,15 @@ async def create_schedule_step_3(update: Update, context: ContextTypes.DEFAULT_T
         schedules: list[ScheduleModel] = repository.get_all_by_date_and_sport(cache_data["day"], cache_data["sport"]["id"])
     exist_schedules = {(schedule.t_start, schedule.t_end)for schedule in schedules}
     available_times = [f"{hour}:00 - {hour + 1}:00" for hour in range(8, 21) if (f"{hour}:00", f"{hour + 1}:00") not in exist_schedules]
-    col_count = 4
-    keyboard = []
-    index = 0
-    for _ in range((len(available_times) // col_count) + 1):
-        row = []
-        for _ in range(col_count):
-            if index >= len(available_times):
-                break
-            trace_id = uuid.uuid4().hex
-            if trace_id not in user_flow_storage[user.id]:
-                user_flow_storage[user.id][trace_id] = {}
-            user_flow_storage[user.id][trace_id].update(cache_data)
-            user_flow_storage[user.id][trace_id]["time"] = available_times[index]
-            row.append(
-                InlineKeyboardButton(
-                    available_times[index], callback_data=CallBackData(tag="cs_3", trace_id=trace_id).model_dump_json()
-                )
-            )
-            index += 1
-        keyboard.append(row)
-    keyboard.append([InlineKeyboardButton("Вернуться в меню", callback_data=CallBackData(tag="back_to_menu").model_dump_json())])
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    keyboard = KeyBoardFactory()
+    for item in available_times:
+        trace_id = uuid.uuid4().hex
+        if trace_id not in user_flow_storage[user.id]:
+            user_flow_storage[user.id][trace_id] = {}
+        user_flow_storage[user.id][trace_id].update(cache_data)
+        user_flow_storage[user.id][trace_id]["time"] = item
+        keyboard.add_item(item, "cs_3", trace_id)
+    reply_markup = InlineKeyboardMarkup(keyboard.generate())
     await query.edit_message_text(text="Выберете время", reply_markup=reply_markup)
     return START_ROUTES
 
@@ -118,10 +90,8 @@ async def create_schedule_step_4(update: Update, context: ContextTypes.DEFAULT_T
     user = update.callback_query.from_user
     cache_data = user_flow_storage[user.id][json.loads(update.callback_query.data)["trace_id"]]
     time_start, time_end = cache_data["time"].split(" - ")
-    keyboard = [
-        [InlineKeyboardButton("Вернуться в меню", callback_data=CallBackData(tag="back_to_menu").model_dump_json())],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    keyboard = KeyBoardFactory()
+    reply_markup = InlineKeyboardMarkup(keyboard.generate())
     flow = UserFlowAdmin()
     flow.create_schedule(cache_data["day"], time_start, time_end, cache_data["sport"]["id"])
     await query.edit_message_text(
---
+++ app/utils/keyboard.py
@@ -0,0 +1,33 @@
+from telegram import InlineKeyboardButton
+
+from app.schemes.callback_data import CallBackData
+
+
+class KeyBoardFactory:
+    COLUMN_COUNT = 4
+
+    def __init__(self):
+        self.keyboard = [[]]
+        self.default_line = [
+            InlineKeyboardButton(
+                "Вернуться в меню",
+                callback_data=CallBackData(tag="back_to_menu").model_dump_json()
+            )
+        ]
+
+    def add_item(self, text: str, tag: str, trace_id: str | None = None):
+        last_row = self.keyboard[-1]
+        if len(last_row) < self.COLUMN_COUNT:
+            last_row.append(
+                InlineKeyboardButton(text, callback_data=CallBackData(tag=tag, trace_id=trace_id).model_dump_json())
+            )
+        else:
+            self.keyboard.append(
+                [InlineKeyboardButton(text, callback_data=CallBackData(tag=tag, trace_id=trace_id).model_dump_json())]
+            )
+    def generate(self):
+        last_row = self.keyboard[-1]
+        if not last_row:
+            self.keyboard.pop(-1)
+        self.keyboard.append(self.default_line)
+        return self.keyboard
